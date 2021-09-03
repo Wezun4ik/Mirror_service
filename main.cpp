f93fe1facb889ab7f608f1269822dec4f0da4103
Wezun4ik <tsupickov.ilya@gmail.com>
2021-09-03 17:45:40 +0300

    enhanced readability: ip::tcp:: -> tcp::

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 using namespace boost::asio;
 using std::cout;
 using std::endl;
+using ip::tcp;
 
 //for debug
 std::ostream& operator <<(std::ostream& stream, std::vector<char>& vec) {
@@ -15,19 +16,19 @@ std::ostream& operator <<(std::ostream& stream, std::vector<char>& vec) {
 }
 
 //temporary empty
-std::vector<char>	read(ip::tcp::socket & socket) {
+std::vector<char>	read(tcp::socket & socket) {
 	return (std::vector<char>{0});
 }
 
 //temporary emptry
-void send(ip::tcp::socket & socket, std::vector<char> message) {
+void send(tcp::socket & socket, std::vector<char> message) {
 }
 
 int main () {
 	//setting up boost.asio
 	io_service			mirror_io_service;
-	ip::tcp::acceptor	mirror_acceptor(mirror_io_service, ip::tcp::endpoint(ip::tcp::v4(), 5875));
-	ip::tcp::socket		mirror_socket(mirror_io_service);
+	tcp::acceptor	mirror_acceptor(mirror_io_service, tcp::endpoint(tcp::v4(), 5875));
+	tcp::socket		mirror_socket(mirror_io_service);
 
 	//accept incoming request
 	mirror_acceptor.accept(mirror_socket);
