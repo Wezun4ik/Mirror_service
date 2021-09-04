8032bc613a3cee14c50baac30e41271b70c7e5fc
Wezun4ik <tsupickov.ilya@gmail.com>
2021-09-04 17:17:50 +0300

    added opencv and image processing; first actual MVP version

--- main.cpp
+++ main.cpp
@@ -2,22 +2,16 @@
 #include <vector>
 #include <boost/asio.hpp>
 #include <boost/regex.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
 
 using namespace boost::asio;
-using std::cout;
-using std::endl;
 using ip::tcp;
-
-//for debug
-std::ostream&		operator <<(std::ostream& stream, std::vector<char>& vec) {
-	//not effective bc for debug purposes
-	for (size_t i = 0; i < vec.size(); i++)
-		stream << vec[i];
-	return (stream);
-}
 
 //dig HTTP header for content length
 size_t				get_content_length(std::string str) {
+	//static for perfomance; not global for readability
 	static const boost::regex	reg_exp("Content-Length: (\\d+)");
 	boost::smatch results;
 
@@ -27,7 +21,7 @@ size_t				get_content_length(std::string str) {
 }
 
 //reads message from client via socket
-std::vector<char>	socket_read(tcp::socket& socket) {
+std::vector<uchar>	socket_read(tcp::socket& socket) {
 	streambuf 					info_buf;
 	std::stringstream			stream;
 
@@ -36,18 +30,36 @@ std::vector<char>	socket_read(tcp::socket& socket) {
 	stream << &info_buf;
 	//read how long is image in bytes
 	auto len_of_image = get_content_length(stream.str());
-	streambuf					image_buf;
-	//read image into vector of chars
+
+	//read image into vector
+	streambuf 					image_buf;
 	boost::asio::read(socket, image_buf, boost::asio::transfer_exactly(len_of_image));
-	std::vector<char>	ret(boost::asio::buffer_cast<const char*>(image_buf.data()),
-							boost::asio::buffer_cast<const char*>(image_buf.data()) + len_of_image);
+	std::vector<uchar>	ret(boost::asio::buffer_cast<const uchar*>(image_buf.data()),
+							boost::asio::buffer_cast<const uchar*>(image_buf.data()) + len_of_image);
 	return (ret);
 }
 
 //sends message to client via socket
-void				socket_send(tcp::socket& socket, std::vector<char>& message) {
+void				socket_send(tcp::socket& socket, std::vector<uchar>& message) {
 	//write to socket; not to be confused with c-style write
 	write(socket, boost::asio::buffer(message));
+}
+
+//image processing
+std::vector<uchar>	mirror_image(std::vector<uchar>& image) {
+	//transform image to IR
+	cv::Mat			raw_data(1, image.size(), CV_8UC1, image.data());
+	cv::Mat			decoded_image  =  cv::imdecode(raw_data , cv::IMREAD_COLOR);
+	//error handling to add later
+
+	//mirror IR-form image
+	cv::Mat			mirror_matrix;
+	flip(decoded_image, mirror_matrix, 1);
+	std::vector<uchar>		ret;
+
+	//write image to buffer
+	cv::imencode(".jpeg", mirror_matrix, ret);
+	return (ret);
 }
 
 int					main() {
@@ -63,8 +75,11 @@ int					main() {
 		//read image from socket
 		auto image = socket_read(mirror_socket);
 
+		//return mirrored image ready to transfer
+		auto mirrored_image = mirror_image(image);
+
 		//send mmirrored image back
-		socket_send(mirror_socket, image);
+		socket_send(mirror_socket, mirrored_image);
 	}
 	return (0);
 }
